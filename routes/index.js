ae68d6e61edc05a9ac2ea0f1d256021a483110db
JIYUN <dlwldbs1026@gmail.com>
2018-03-22 16:00:07 +0900

    /get shows all dump data

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var http = require('http');
 
 router.get('/', function(req, res) {
     res.render('index', {title: '20131144 JIYUN LEE | Coursework #6', profile: undefined});
@@ -11,26 +10,9 @@ router.get('/', function(req, res) {
 router.get('/update', function(req, res) {
     var api_key = req.param('api_key');
     var field1 = req.param('field1');
-    var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-    var body = date + ' / ' + 'api_key=' + api_key + '&field1=' + field1 + '\n';
+    var date = new Date().toISOString().replace(/T/, '_').replace(/\..+/, '');
+    var body = date + ',' + api_key + ',' + field1 + '\n';
 
-    filePath = 'data.txt';
-    req.on('data', function(data) {
-        body += (data + '\n');
-    });
-
-    req.on('end', function() {
-        fs.appendFile(filePath, body, function() {
-            res.end();
-        });
-    });
-    res.redirect('/');
-});
-
-// 메인페이지에서 form을 통해 덤프 데이터 입력
-router.post('/update', function(req, res) {
-    var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-    var body = date + ' / ';
     filePath = 'data.txt';
     req.on('data', function(data) {
         body += (data + '\n');
@@ -46,12 +28,17 @@ router.post('/update', function(req, res) {
 
 // 가장 최근의 n개의 덤프 데이터 화면에 출력
 router.get('/get', function(req, res) {
-    var tmp = req.param('count');
-    console.log(tmp);
-
+    var tmp;
     var data = fs.readFileSync('./data.txt', 'utf8');
     var lines = data.split('\n');
     var total_line = lines.length-1;
+
+    if (req.param('count')) {
+        tmp = req.param('count');
+        console.log(tmp);
+    } else {
+        tmp = total_line;
+    }
 
     function get_line(filename, line_no, callback) {
         var data = fs.readFileSync(filename, 'utf8');
@@ -75,17 +62,17 @@ router.get('/get', function(req, res) {
     res.render('get', {title: '20131144 JIYUN LEE | DUMP DATAS', profile: undefined, value: tmp, result: result});
 });
 
-// 1시간마다 랜덤으로 덤프 데이터 생성
+// 일정시간마다 랜덤으로 덤프 데이터 생성
 setInterval(function() {
     var random_string = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
-    var random_number = (Math.random() * (40.00 - 0.01) + 0.01).toFixed(2);
-    var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-    var body = date + ' / ' + 'api_key=' + random_string + '&field1=' + random_number + '\n';
+    var random_number = (Math.random() * (30.00 - 15.00) + 15.00).toFixed(2);
+    var date = new Date().toISOString().replace(/T/, '_').replace(/\..+/, '');
+    var body = date + ',' + random_string + ',' + random_number + '\n';
 
     fs.appendFile('data.txt', body, function (err) {
         if (err) throw err;
         console.log(body)
     });
-}, 60 * 60 * 1000);
+}, 10 * 1000);
 
 module.exports = router;
